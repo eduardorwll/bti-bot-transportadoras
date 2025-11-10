2b0784cb019272adcd01355710267e3f9d0edaf6
Eduardo Rewell <dudurewell@gmail.com>
2025-11-09 23:12:15 -0300

    working on modularization

--- mod/Montador de contexto/caso_interativa.js
+++ mod/Montador de contexto/caso_interativa.js
@@ -1,6 +1,6 @@
 // Variáveis de ambiente
 const idWhatsapp = $input.first().json.id_whatsapp
-const idRespostaInterativa = $input.first().json.id_resposta_interativa
+const idRespostaInterativa = parseInt($input.first().json.id_resposta_interativa)
 const descricaoRespostaInterativa = $input.first().json.descricao_resposta_interativa
 const respostaTexto = $input.first().json.resposta_texto
 const estadoAtual = $input.first().json.estado_atual
@@ -21,5 +21,9 @@ if (idRespostaInterativa === 998) {
 
 switch (estadoAtual) {
     case 'BEM_VINDO':
-        
+        return {
+            proximo_estado: 'MENU_PRINCIPAL'
+        }
+    case 'MENU_PRINCIPAL':
+        return
 }
